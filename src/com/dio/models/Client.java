1e9b847492d9be2fe9644e5801c142db6970cb91
LuisD <luis.domiciano@hotmail.com>
2022-07-03 22:26:11 -0300

    feat: model Client

---
+++ src/com/dio/models/Client.java
@@ -0,0 +1,36 @@
+package com.dio.models;
+
+public class Client {
+
+	private String name;
+	private String email;
+	private String cpf;
+	
+	public Client() {
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+}
